ad0261a8f52d9f8c070b26f92dfe9734091765c7
Sourish Ghosh <ragnarok0211@gmail.com>
2015-10-06 01:19:00 +0530

    Update code

--- bot-detect.cpp
+++ bot-detect.cpp
@@ -2,57 +2,51 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/core/core.hpp"
 #include <bits/stdc++.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <ctime>
 
 using namespace cv;
 using namespace std;
 
-Mat src, imgHSV, head, tail, digits;
+Mat src2, imgHSV, head, tail;
 
-int h11 = 59;
-int s11 = 79;
-int v11 = 58;
-int h12 = 128;
-int s12 = 138;
-int v12 = 226;
+int h11 = 154;
+int s11 = 67;
+int v11 = 62;
+int h12 = 190;
+int s12 = 294;
+int v12 = 302;
 
-int h21 = 116;
-int s21 = 141;
-int v21 = 153;
-int h22 = 238;
-int s22 = 243;
-int v22 = 268;
-
-int h31 = 0;
-int s31 = 0;
-int v31 = 201;
-int h32 = 146;
-int s32 = 26;
-int v32 = 270;
+int h21 = 65;
+int s21 = 193;
+int v21 = 207;
+int h22 = 243;
+int s22 = 276;
+int v22 = 286;
 
 const int TMAX = 350;
+char state;
+int fd;
+Point finalLoc(400, 200);
 
 void init_trackbars() {
 	namedWindow("Head", 1);
 	namedWindow("Tail", 1);
-	// namedWindow("Digits", 1);
-	// createTrackbar("H1", "Head", &h11, TMAX);
-	// createTrackbar("S1", "Head", &s11, TMAX);
-	// createTrackbar("V1", "Head", &v11, TMAX);
-	// createTrackbar("H2", "Head", &h12, TMAX);
-	// createTrackbar("S2", "Head", &s12, TMAX);
-	// createTrackbar("V2", "Head", &v12, TMAX);
-	// createTrackbar("H1", "Tail", &h21, TMAX); 
-	// createTrackbar("S1", "Tail", &s21, TMAX);
-	// createTrackbar("V1", "Tail", &v21, TMAX);
-	// createTrackbar("H2", "Tail", &h22, TMAX);
-	// createTrackbar("S2", "Tail", &s22, TMAX);
-	// createTrackbar("V2", "Tail", &v22, TMAX);
-	// createTrackbar("H1", "Digits", &h31, TMAX); 
-	// createTrackbar("S1", "Digits", &s31, TMAX);
-	// createTrackbar("V1", "Digits", &v31, TMAX);
-	// createTrackbar("H2", "Digits", &h32, TMAX);
-	// createTrackbar("S2", "Digits", &s32, TMAX);
-	// createTrackbar("V2", "Digits", &v32, TMAX);
+	createTrackbar("H1", "Head", &h11, TMAX);
+	createTrackbar("S1", "Head", &s11, TMAX);
+	createTrackbar("V1", "Head", &v11, TMAX);
+	createTrackbar("H2", "Head", &h12, TMAX);
+	createTrackbar("S2", "Head", &s12, TMAX);
+	createTrackbar("V2", "Head", &v12, TMAX);
+	createTrackbar("H1", "Tail", &h21, TMAX); 
+	createTrackbar("S1", "Tail", &s21, TMAX);
+	createTrackbar("V1", "Tail", &v21, TMAX);
+	createTrackbar("H2", "Tail", &h22, TMAX);
+	createTrackbar("S2", "Tail", &s22, TMAX);
+	createTrackbar("V2", "Tail", &v22, TMAX);
 }
 
 Point find_center(Mat img, Mat& drawing, Scalar col) {
@@ -75,30 +69,72 @@ Point find_center(Mat img, Mat& drawing, Scalar col) {
 	circle(drawing, p, 5, col, CV_FILLED, 8, 0);
 	return p;
 }
+void sendCommand(const char* command) {
+	if (command[0] != state) {
+		write(fd, command, 1);
+		state = command[0];
+		cout << "sending " << state << endl;
+	}
+}
+
+float dist(Point p1, Point p2) { return sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y)); }
 
 int main(int argc, char const *argv[])
 {
+	state = 'b';
+    fd = open("/dev/ttyACM1", O_RDWR | O_NOCTTY);  //Opening device file
+    printf("fd opened as %i\n", fd);
+   	usleep(2500000);
 	init_trackbars();
-	VideoCapture cap("vids/vid3.wmv");
-	cap >> src;
-	Mat drawing(src.rows, src.cols, CV_8UC3, Scalar(0, 0, 0));
+	VideoCapture cap(1);
 	while (1) {
-		cvtColor(src, imgHSV, CV_BGR2HSV);
+		cap >> src2;
+		Mat drawing(src2.rows, src2.cols, CV_8UC3, Scalar(0, 0, 0));
+		circle(drawing, finalLoc, 5, Scalar(22, 44, 66), CV_FILLED, 8, 0);
+		cvtColor(src2, imgHSV, CV_BGR2HSV);
 		inRange(imgHSV, Scalar(h11, s11, v11), Scalar(h12, s12, v12), head);
 		inRange(imgHSV, Scalar(h21, s21, v21), Scalar(h22, s22, v22), tail);
 		inRange(imgHSV, Scalar(h31, s31, v31), Scalar(h32, s32, v32), digits);
-		// imshow("HEAD", head);
-		// imshow("TAIL", tail);
-		//imshow("DIGITS", digits);
+		imshow("HEAD", head);
+		imshow("TAIL", tail);
+		// imshow("DIGITS", digits);
 		Point c1 = find_center(head, drawing, Scalar(255, 0, 0));
 		Point c2 = find_center(tail, drawing, Scalar(255, 255, 15));
+		Point botLoc((c1.x + c2.x) / 2, (c1.y + c2.y) / 2);
 
-		float angle = atan((float)(c2.y - c1.y) / (float)(c2.x - c1.x)) * 180. / 3.14;
-		cout << angle << endl;
+		float m1 = (float)(c2.y - c1.y) / (float)(c2.x - c1.x);
+		float m2 = (float)(finalLoc.y - c1.y) / (float)(finalLoc.x - c1.x);
+		float anglediff = atan((m1 - m2) / (1.0 + m1*m2)) * 180. / 3.14;
+		cout << "Current Bot: " << botLoc.x << ", " << botLoc.y << endl;
+		cout << "GOTO: " << finalLoc.x << ", " << finalLoc.y << endl;
+		cout << "Angle Diff: " << anglediff << endl;
 
-		imshow("Main Vid", src);
+		if (fabs(anglediff) > 20) {
+			sendCommand("a");
+		} else {
+			cout << "------------------------------------" << endl;
+			sendCommand("b");
+		}
+
+		if (!(state == 'a' || state == 'd')) {
+			if (dist(botLoc, finalLoc) < 30) {
+				sendCommand("b");
+			} else {
+				if (dist(c1, finalLoc) < dist(c2, finalLoc)) {
+					sendCommand("w");
+				} else {
+					sendCommand("s");
+				}
+			}
+		}
+
+		if (dist(botLoc, finalLoc) < 20) {
+			sendCommand("b");
+		}
+
+		imshow("Main Vid", src2);
 		imshow("Dots", drawing);
-		if (waitKey(30) >= 0) {
+		if (waitKey(60) >= 0) {
 			break;
 		}
 	}
--- char-detect.cpp
+++ char-detect.cpp
@@ -3,6 +3,11 @@
 #include <tesseract/baseapi.h>
 #include <tesseract/strngs.h>
 #include <bits/stdc++.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <ctime>
 
 using namespace cv;
 using namespace std;
@@ -11,11 +16,79 @@ vector<pair<char, pair<int, int> > > expr_digits;
 vector<pair<char, pair<int, int> > > expr_ops;
 string expr;
 
-Mat src; Mat src_gray;
-int thresh = 100;
+Mat src; Mat src_gray; Mat src_binary;
+int thresh = 228;
 int max_thresh = 255;
 RNG rng(12345);
 tesseract::TessBaseAPI tess;
+
+Mat src2, imgHSV, head, tail;
+
+int h11 = 154;
+int s11 = 67;
+int v11 = 62;
+int h12 = 190;
+int s12 = 294;
+int v12 = 302;
+
+int h21 = 65;
+int s21 = 193;
+int v21 = 207;
+int h22 = 243;
+int s22 = 276;
+int v22 = 286;
+
+const int TMAX = 350;
+char state;
+int fd;
+Point finalLoc(400, 200);
+
+void init_trackbars() {
+	namedWindow("Head", 1);
+	namedWindow("Tail", 1);
+	createTrackbar("H1", "Head", &h11, TMAX);
+	createTrackbar("S1", "Head", &s11, TMAX);
+	createTrackbar("V1", "Head", &v11, TMAX);
+	createTrackbar("H2", "Head", &h12, TMAX);
+	createTrackbar("S2", "Head", &s12, TMAX);
+	createTrackbar("V2", "Head", &v12, TMAX);
+	createTrackbar("H1", "Tail", &h21, TMAX); 
+	createTrackbar("S1", "Tail", &s21, TMAX);
+	createTrackbar("V1", "Tail", &v21, TMAX);
+	createTrackbar("H2", "Tail", &h22, TMAX);
+	createTrackbar("S2", "Tail", &s22, TMAX);
+	createTrackbar("V2", "Tail", &v22, TMAX);
+}
+
+Point find_center(Mat img, Mat& drawing, Scalar col) {
+	vector<vector<Point> > contours;
+	vector<Vec4i> hierarchy;
+	Rect bounding_rect;
+	int largest_area = 0; 
+	findContours(img, contours, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE);
+	for(int i = 0; i< contours.size(); i++)
+	{
+    	double a = contourArea(contours[i], false); 
+		if(a > largest_area){
+			largest_area = a;
+			bounding_rect = boundingRect(contours[i]); 
+		}
+	}
+	Point p1 = bounding_rect.tl();
+	Point p2 = bounding_rect.br();
+	Point p((p1.x + p2.x)/2, (p1.y + p2.y)/2);
+	circle(drawing, p, 5, col, CV_FILLED, 8, 0);
+	return p;
+}
+void sendCommand(const char* command) {
+	if (command[0] != state) {
+		write(fd, command, 1);
+		state = command[0];
+		cout << "sending " << state << endl;
+	}
+}
+
+float dist(Point p1, Point p2) { return sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y)); }
 
 /// Function header
 void thresh_callback(int, void*);
@@ -79,13 +152,14 @@ int evaluate_epxr(const char *A) {
 }
 
 void predictDigit(Rect rr) {
-	Mat sub = src_gray(rr);
-	tess.SetImage((uchar*)sub.data, sub.size().width, sub.size().height, sub.channels(), sub.step1());
+	//Mat sub = src_gray(rr);
+	tess.SetImage((uchar*)src_binary.data, src_binary.size().width, src_binary.size().height, src_binary.channels(), src_binary.step1());
 	tess.Recognize(0);
 	char* out = tess.GetUTF8Text();
 	char c = out[0];
 	int cx = (rr.tl().x + rr.br().x) / 2;
 	int cy = (rr.tl().y + rr.br().y) / 2;
+	cout << c << endl;
 	if (c == 'X' || c == 'x') c = '*';
 	if (c >= '0' && c <= '9')
 		expr_digits.push_back(make_pair(c, make_pair(cx, cy)));
@@ -96,9 +170,12 @@ void predictDigit(Rect rr) {
 /** @function main */
 int main( int argc, char** argv )
 {
+	VideoCapture cap(1);
 	tess.Init(NULL, "eng", tesseract::OEM_DEFAULT);
 	tess.SetPageSegMode(tesseract::PSM_SINGLE_BLOCK);
-	src = imread("img/sheldon_data.jpg", 1 );
+	//src = imread("img/sample1.jpg", 1 );
+	cap >> src;
+	src_binary = src;
 	cvtColor( src, src_gray, CV_BGR2GRAY );
 	blur( src_gray, src_gray, Size(3,3) );
 	namedWindow( "source_window", CV_WINDOW_AUTOSIZE );
@@ -145,8 +222,76 @@ int main( int argc, char** argv )
 			}
 		}
 	}
-	// for(int i = 0; i < goToQ.size(); i++)
-	// 	cout << goToQ[i].first << ", " << goToQ[i].second << endl;
+	for(int i = 0; i < goToQ.size(); i++)
+		cout << goToQ[i].first << ", " << goToQ[i].second << endl;
+
+	state = 'b';
+    fd = open("/dev/ttyACM1", O_RDWR | O_NOCTTY);  //Opening device file
+    printf("fd opened as %i\n", fd);
+   	usleep(2500000);
+	init_trackbars();
+	finalLoc.x = goToQ[0].first;
+	finalLoc.y = goToQ[0].second;
+	int vec_idx = 0;
+	while (1) {
+		cap >> src2;
+		Mat drawing(src2.rows, src2.cols, CV_8UC3, Scalar(0, 0, 0));
+		circle(drawing, finalLoc, 5, Scalar(22, 44, 66), CV_FILLED, 8, 0);
+		cvtColor(src2, imgHSV, CV_BGR2HSV);
+		inRange(imgHSV, Scalar(h11, s11, v11), Scalar(h12, s12, v12), head);
+		inRange(imgHSV, Scalar(h21, s21, v21), Scalar(h22, s22, v22), tail);
+		imshow("HEAD", head);
+		imshow("TAIL", tail);
+		// imshow("DIGITS", digits);
+		Point c1 = find_center(head, drawing, Scalar(255, 0, 0));
+		Point c2 = find_center(tail, drawing, Scalar(255, 255, 15));
+		Point botLoc((c1.x + c2.x) / 2, (c1.y + c2.y) / 2);
+
+		float m1 = (float)(c2.y - c1.y) / (float)(c2.x - c1.x);
+		float m2 = (float)(finalLoc.y - c1.y) / (float)(finalLoc.x - c1.x);
+		float anglediff = atan((m1 - m2) / (1.0 + m1*m2)) * 180. / 3.14;
+		cout << "Current Bot: " << botLoc.x << ", " << botLoc.y << endl;
+		cout << "GOTO: " << finalLoc.x << ", " << finalLoc.y << endl;
+		cout << "Angle Diff: " << anglediff << endl;
+		cout << "BOT DIST: " << dist(botLoc, finalLoc) << endl;
+
+		if (fabs(anglediff) > 20) {
+			sendCommand("a");
+		} else {
+			cout << "------------------------------------" << endl;
+			sendCommand("b");
+		}
+
+		if (!(state == 'a' || state == 'd')) {
+			if (dist(botLoc, finalLoc) < 30) {
+				sendCommand("b");
+			} else {
+				if (dist(c1, finalLoc) < dist(c2, finalLoc)) {
+					sendCommand("w");
+				} else {
+					sendCommand("s");
+				}
+			}
+		}
+
+		if (dist(botLoc, finalLoc) < 40) {
+			sendCommand("b");
+			sendCommand("l");
+			usleep(3000000);
+			vec_idx++;
+			finalLoc.x = goToQ[vec_idx].first;
+			finalLoc.y = goToQ[vec_idx].second;
+			if (vec_idx > goToQ.size())
+				return 0;
+		}
+
+		imshow("Main Vid", src2);
+		imshow("Dots", drawing);
+		if (waitKey(60) >= 0) {
+			break;
+		}
+	}
+
 	waitKey(0);
 	return(0);
 }
@@ -170,7 +315,8 @@ void thresh_callback(int, void* )
 	{
 		approxPolyDP( Mat(contours[i]), contours_poly[i], 3, true );
 		double a = contourArea(contours[i]);
-		if (a >= 3800 && a <= 4000)
+		cout << a << endl;
+		if (a >= 300 && a <= 800)
 			boundRect.push_back(boundingRect( Mat(contours_poly[i]) ));
 	}
 	/// Draw polygonal contour + bonding rects + circles
@@ -180,7 +326,13 @@ void thresh_callback(int, void* )
 	{
 		Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255));
 		rectangle(drawing, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0);
-		//zz = src_gray(boundRect[i]);
+		zz = src_gray(boundRect[i]);
+		Mat binaryMat(zz.size(), zz.type());
+		threshold(zz, binaryMat, 150, 255, THRESH_BINARY);
+		src_binary.rows = binaryMat.rows;
+		src_binary.cols = binaryMat.cols;
+		src_binary = binaryMat;
+		imshow("sample" + i, src_binary);
 		predictDigit(boundRect[i]);
 	}
 	/// Show in a window
